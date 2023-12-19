9e2730d57af6314a4a02744ddb59668f24159c3e
pedrocesarrp <pedro.passos@daedalus-tech.com>
2023-12-18 22:59:58 -0300

    add first problem

---
+++ 88. Merge Sorted Array/resolution.py
@@ -0,0 +1,29 @@
+
+
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+m = 3
+n = 3
+
+def merge(nums1, m, nums2, n):
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    final = []
+    # i = 0
+    # j = 0
+    
+    for i in range(m):
+        for j in range(n):
+            print(f'')
+
+
+
+
+    print(final)
+
+
+merge(nums1,m, nums2, n)
+    
+
+
